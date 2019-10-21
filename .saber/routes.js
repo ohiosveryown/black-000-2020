7c811080ec5e18669b6a77d756df00b4a87dabb0
Matthew Pence <matt@ohiosveryown.co>
2019-10-21 19:42:12 -0400

    Post tests

--- .saber/routes.js
+++ .saber/routes.js
@@ -1,54 +1,6 @@
 
     export default [
       {
-              path: "/posts/test-four.html",
-              meta: {
-                __relative: '_posts/test-four.md',
-                __pageId: '69376bf2'
-              },
-              component: function() {
-                
-                return import(/* webpackChunkName: "page--_posts-test-four-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/_posts/test-four.md?saberPage=69376bf2")
-                
-              }
-            },
-{
-              path: "/posts/test-one.html",
-              meta: {
-                __relative: '_posts/test-one.md',
-                __pageId: 'fda08312'
-              },
-              component: function() {
-                
-                return import(/* webpackChunkName: "page--_posts-test-one-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/_posts/test-one.md?saberPage=fda08312")
-                
-              }
-            },
-{
-              path: "/posts/test-three.html",
-              meta: {
-                __relative: '_posts/test-three.md',
-                __pageId: '5127b382'
-              },
-              component: function() {
-                
-                return import(/* webpackChunkName: "page--_posts-test-three-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/_posts/test-three.md?saberPage=5127b382")
-                
-              }
-            },
-{
-              path: "/posts/test-two.html",
-              meta: {
-                __relative: '_posts/test-two.md',
-                __pageId: 'eb894e5e'
-              },
-              component: function() {
-                
-                return import(/* webpackChunkName: "page--_posts-test-two-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/_posts/test-two.md?saberPage=eb894e5e")
-                
-              }
-            },
-{
               path: "/",
               meta: {
                 __relative: 'index.md',
@@ -57,6 +9,54 @@
               component: function() {
                 
                 return import(/* webpackChunkName: "page--index-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/index.md?saberPage=26bb90aa")
+                
+              }
+            },
+{
+              path: "/posts/delete-one.html",
+              meta: {
+                __relative: '_posts/delete-one.md',
+                __pageId: '5fc703fe'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-delete-one-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/_posts/delete-one.md?saberPage=5fc703fe")
+                
+              }
+            },
+{
+              path: "/posts/delete-two.html",
+              meta: {
+                __relative: '_posts/delete-two.md',
+                __pageId: '68d29e58'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-delete-two-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/_posts/delete-two.md?saberPage=68d29e58")
+                
+              }
+            },
+{
+              path: "/posts/delete-three.html",
+              meta: {
+                __relative: '_posts/delete-three.md',
+                __pageId: '6d482606'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-delete-three-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/_posts/delete-three.md?saberPage=6d482606")
+                
+              }
+            },
+{
+              path: "/posts/delete-four.html",
+              meta: {
+                __relative: '_posts/delete-four.md',
+                __pageId: '80949540'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-delete-four-md" */ "/Users/pence/Desktop/projects/black-000/black-000-2020/pages/_posts/delete-four.md?saberPage=80949540")
                 
               }
             },
