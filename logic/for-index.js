39b9c1817bc77e7fe7e0cb9762435e6f4f20d9e7
ohiosveryown <matt@ohiosveryown.co>
2019-11-01 13:15:29 -0400

    Testing index to detail sm transition

--- logic/for-index.js
+++ logic/for-index.js
@@ -7,10 +7,10 @@ const leaveLogic = () => {
   const mq = window.matchMedia( '(min-width: 700px)' )
   const cover = document.querySelector('.cover')
   // styles
-  cover.style.cssText = `
-    transform: scale(1,1) translate(0,0);
-    transition: all var(--animbase) var(--inout);
-  `
+  // cover.style.cssText = `
+  //   transform: scale(1,1) translate(0,0);
+  //   transition: all var(--animbase) var(--inout);
+  // `
   // for ≥ md
   if (mq.matches) {
     cover.style.cssText = `
